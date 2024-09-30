17b0ae2ba5ff55435dd8a843e5a8358ea1dedc88
Esteban Samayoa Castillo <61565155+estebansamayoa1@users.noreply.github.com>
2024-09-30 11:12:38 -0600

    dockerfile y modificacion para docker

--- ToDoApp/app.py
+++ ToDoApp/app.py
@@ -48,5 +48,5 @@ def delete(id):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Crear las tablas si no existen
-    app.run(debug=True)
+        db.create_all() 
+    app.run(host='0.0.0.0', port=8080)
