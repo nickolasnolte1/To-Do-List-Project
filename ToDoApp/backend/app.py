e1fd8772c20a98d61aa98b9422bab06783f91e2f
Esteban Samayoa Castillo <61565155+estebansamayoa1@users.noreply.github.com>
2024-09-30 17:47:32 -0600

    agregar base de datos (postgres)

--- ToDoApp/backend/app.py
+++ ToDoApp/backend/app.py
@@ -1,28 +1,33 @@
+import os
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
-# Configure the SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+database_url = os.getenv('DATABASE_URL')
+if not database_url:
+    raise RuntimeError("DATABASE_URL is not set. Flask cannot connect to a database!")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Define the Todo model
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-# Route to fetch all tasks
+# API route to fetch all tasks
 @app.route('/api/todos', methods=['GET'])
 def get_tasks():
     tasks = Todo.query.all()
     tasks_list = [{'id': task.id, 'title': task.title} for task in tasks]
     return jsonify(tasks_list)
 
-# Route to add a task
+# API route to add a new task
 @app.route('/api/add', methods=['POST'])
 def add_task():
     title = request.form.get('title')
@@ -32,7 +37,7 @@ def add_task():
         db.session.commit()
     return jsonify({"message": "Task added successfully"})
 
-# Route to delete a task
+# API route to delete a task
 @app.route('/api/delete/<int:id>', methods=['DELETE'])
 def delete_task(id):
     task_to_delete = Todo.query.get_or_404(id)
@@ -42,5 +47,5 @@ def delete_task(id):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Create database tables if they don't exist
+        db.create_all()  # Create database tables in PostgreSQL if they don't exist
     app.run(host='0.0.0.0', port=5050)
